110df5ef3dcb89098d7c28c368e6f3102900bc55
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-08-24 18:48:42 +0100

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,1 +1,6 @@
-window.addEventListener("message", console.info);
+'use strict';
+var script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.setAttribute('crossorigin', 'anonymous');
+script.setAttribute('src', chrome.runtime.getURL('WindowScript.js'));
+document.head.appendChild(script);
