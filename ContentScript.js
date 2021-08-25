a12e6057baca0c103f503e09599457decd09df95
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-08-25 16:44:32 +0100

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,7 +1,13 @@
 'use strict';
 window.addEventListener("message", e => console.info(location.origin, "received", e.data, "from origin", e.origin));
-var script = document.createElement('script');
-script.setAttribute('type', 'text/javascript');
-script.setAttribute('crossorigin', 'anonymous');
-script.setAttribute('src', chrome.runtime.getURL('WindowScript.js'));
-document.head.appendChild(script);
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Adds content to DOM needed because of isolation
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('crossorigin', 'anonymous');
+  script.setAttribute('src', chrome.runtime.getURL('WindowScript.js'));
+  document.head.appendChild(script);
+}, {
+  passive: true
+});
