17bd5006f32cb51a5a8a13c68e35cd9b3af57ce5
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-08-25 16:33:48 +0100

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -6,21 +6,25 @@ const handler = {
   get: function(target, property) {
     if (property !== "postMessage") return target[property];
     return function() {
-      hook(arguments);
+      hook(arguments, target);
       target[property].apply(target, arguments);
     }
   },
 };
 
-window.opener = new Proxy(realOpener, handler);
-window.parent = new Proxy(realParent, handler);
+if (realOpener) window.opener = new Proxy(realOpener, handler);
+if (realParent) window.parent = new Proxy(realParent, handler);
 
 window.postMessage = function() {
-  hook(arguments);
+  hook(arguments, window);
   realPost.apply(this, arguments);
 }
 
 
-function hook(data) {
-  console.log(data);
+function hook(data, target) {
+  if (target === window) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to self");
+  if (target === window.opener && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to opener");
+  if (target === window.opener) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to opener");
+  if (target === window.parent && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to parent");
+  if (target === window.parent) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to parent");
 }
