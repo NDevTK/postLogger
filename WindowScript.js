b3eda417788fe02afa4df6e69de577ac86c75bd8
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-08-26 00:08:43 +0100

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -1,30 +1,63 @@
-let realOpener = window.opener;
-let realParent = window.parent;
-let realPost = window.postMessage;
+const windows = new Map();
 
 const handler = {
   get: function(target, property) {
     if (property !== "postMessage") return target[property];
     return function() {
       hook(arguments, target);
-      target[property].apply(target, arguments);
+      let result = target[property].apply(target, arguments);
+      hookWindow(result.source);
+      return result;
     }
   },
 };
 
-if (realOpener) window.opener = new Proxy(realOpener, handler);
-if (realParent) window.parent = new Proxy(realParent, handler);
-
-window.postMessage = function() {
-  hook(arguments, window);
-  realPost.apply(this, arguments);
+function hookWindow(w) {
+  if (!w) return;
+  
+  if (w.opener) {
+    let real = w.opener;
+    if (windows.has(real)) {
+      w.opener = windows.get(real);
+    } else {
+      w.opener = new Proxy(real, handler);
+      windows.set(real, w);
+    }
+    real.postLoggerType = "opener;
+  }
+  
+  if (w.parent) {
+    let real = w.parent;
+    if (windows.has(real)) {
+      w.parent = windows.get(real);
+    } else {
+      w.parent = new Proxy(real, handler);
+      windows.set(real, w);
+    }
+    real.postLoggerType = "parent;
+  }
+  
+  if (w) {
+    let real = w.postMessage;
+    if (windows.has(real)) {
+      w.postMessage = windows.get(real);
+    } else {
+      w.postMessage = function() {
+        hook(arguments);
+        real.apply(this, arguments);
+      }
+      windows.set(real, w);
+    }
+    real.postLoggerType = "self;
+  }
 }
 
+hookWindow(window);
 
 function hook(data, target) {
-  if (target === window) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to self");
-  if (target === realOpener && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to opener");
-  if (target === realOpener) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to opener");
-  if (target === realParent && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to parent");
-  if (target === realParent) return console.info(location.origin, "sent", data[0], "with scope", data[1], "to parent");
+  if (target.postLoggerType === "self") return console.info(location.origin, "sent", data[0], "with scope", data[1], "to self");
+  if (target.postLoggerType === "opener" && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to opener");
+  if (target.postLoggerType === "opener") return console.info(location.origin, "sent", data[0], "with scope", data[1], "to opener");
+  if (target.postLoggerType === "parent" && data[1] === "*") return console.warn(location.origin, "sent", data[0], "with scope", data[1], "to parent");
+  if (target.postLoggerType === "parent") return console.info(location.origin, "sent", data[0], "with scope", data[1], "to parent");
 }
