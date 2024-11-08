235aa76dec6f49beacc1333385d56fb022721155
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-11-08 06:37:14 +0000

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -1,114 +1,114 @@
 // Code here is exposed to the website.
 (function() {
-'use strict';
+    'use strict';
 
-const windows = new Map();
-const iframes = new Set();
+    const windows = new Map();
+    const iframes = new Set();
 
-function whois(win, origin) {
- if (win === window.top) return 'top (' + origin + ')';
- if (win === window.parent && win !== window) return 'parent (' + origin + ')';
- if (win === window.opener) return 'opener (' + origin + ')';
- 
- if (win.opener === window && win === win.top) return 'popup (' + origin + ')';
- if (win.opener === window && win !== win.top) return 'popup iframe (' + origin + ')';
- 
- if (win.opener?.opener === window) return 'opener of opener (' + origin + ')';
- if (win.opener?.parent === window && win.opener?.parent !== win.opener) return 'parent of opener (' + origin + ')';
- 
- if (win.top === window.top && win.parent !== window.top) return 'nested iframe (' + origin + ')';
- if (win.top === window.top && win.parent === window.top) return 'iframe (' + origin + ')';
- return 'other (' + origin + ')';
-}
+    function whois(win, origin) {
+        if (win === window.top) return 'top (' + origin + ')';
+        if (win === window.parent && win !== window) return 'parent (' + origin + ')';
+        if (win === window.opener) return 'opener (' + origin + ')';
 
-const me = whois(window, window.origin);
+        if (win.opener === window && win === win.top) return 'popup (' + origin + ')';
+        if (win.opener === window && win !== win.top) return 'popup iframe (' + origin + ')';
 
-function hook(data, type, iframe) {
-  let scope = data[1];
-  let message = data[0];
-  // If omitted, then defaults to the origin that is calling the method.
-  if (!scope) scope = window.origin;
-  if (type === "self") return console.info(me, "sent", message, "with scope", scope, "to self");
-  if (type === "opener" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to opener");
-  if (type === "opener") return console.info(me, "sent", message, "with scope", scope, "to opener");
-  if (type === "iframe" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to iframe", iframe);
-  if (type === "iframe") return console.info(me, "sent", message, "with scope", scope, "to iframe", iframe);
-  if (type === "parent" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to parent");
-  if (type=== "parent") return console.info(me, "sent", message, "with scope", scope, "to parent");
-}
+        if (win.opener?.opener === window) return 'opener of opener (' + origin + ')';
+        if (win.opener?.parent === window && win.opener?.parent !== win.opener) return 'parent of opener (' + origin + ')';
 
-window.addEventListener("message", e => {
- console.info(me, "received", e.data, "from", whois(e.source, e.origin));
-});
-
-function hookIframe(iframe) {
-  if (iframes.has(iframe)) return;
-  const iframeProxy = {
-    get(target, prop, receiver) {
-      let result = Reflect.get(...arguments);
-      if (prop !== 'contentWindow') return result;
-      return new Proxy(result, handle('iframe', iframe));
-    },
-  };
-  try {
-  iframe.__proto__ = new Proxy(iframe.__proto__, iframeProxy);
-  iframes.add(iframe);
-  } catch {}
-}
-
-setInterval(() => {
-  document.querySelectorAll('iframe').forEach(hookIframe);
-}, 100);
-
-function handle(type, iframe) {
-  return {
-    get: function(target, property) {
-      if (property !== "postMessage") return Reflect.get(...arguments);
-      return function() {
-        hook(arguments, type, iframe);
-        return target[property].apply(target, arguments);
-      }
-    },
-  };
-}
-
-function hasProperty(value, key) {
-  return Object.prototype.hasOwnProperty.call(value, key);
-}
-
-function hookWindow(w, p) {
-  if (hasProperty(w, p)) {
-    if (!(w[p] instanceof Window)) return;
-    let real = w[p];
-    if (windows.has(real)) {
-      w[p] = windows.get(real);
-    } else {
-      w[p] = new Proxy(real, handle(p));
-      windows.set(real, w);
+        if (win.top === window.top && win.parent !== window.top) return 'nested iframe (' + origin + ')';
+        if (win.top === window.top && win.parent === window.top) return 'iframe (' + origin + ')';
+        return 'other (' + origin + ')';
     }
-  }
-}
 
-function hookWindows(w) {
-  if (!(w instanceof Window)) return;
+    const me = whois(window, window.origin);
 
-  hookWindow(w, "parent");
-  hookWindow(w, "opener");
-
-  if (hasProperty(w, "postMessage")) {
-    let real = w.postMessage;
-    if (windows.has(real)) {
-      w.postMessage = windows.get(real);
-    } else {
-      w.postMessage = function() {
-        hook(arguments, "self");
-        real.apply(this, arguments);
-      }
-      windows.set(real, w);
+    function hook(data, type, iframe) {
+        let scope = data[1];
+        let message = data[0];
+        // If omitted, then defaults to the origin that is calling the method.
+        if (!scope) scope = window.origin;
+        if (type === "self") return console.info(me, "sent", message, "with scope", scope, "to self");
+        if (type === "opener" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to opener");
+        if (type === "opener") return console.info(me, "sent", message, "with scope", scope, "to opener");
+        if (type === "iframe" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to iframe", iframe);
+        if (type === "iframe") return console.info(me, "sent", message, "with scope", scope, "to iframe", iframe);
+        if (type === "parent" && scope === "*") return console.warn(me, "sent", message, "with scope", scope, "to parent");
+        if (type === "parent") return console.info(me, "sent", message, "with scope", scope, "to parent");
     }
-  }
-}
 
-hookWindows(window);
+    window.addEventListener("message", e => {
+        console.info(me, "received", e.data, "from", whois(e.source, e.origin));
+    });
+
+    function hookIframe(iframe) {
+        if (iframes.has(iframe)) return;
+        const iframeProxy = {
+            get(target, prop, receiver) {
+                let result = Reflect.get(...arguments);
+                if (prop !== 'contentWindow') return result;
+                return new Proxy(result, handle('iframe', iframe));
+            },
+        };
+        try {
+            iframe.__proto__ = new Proxy(iframe.__proto__, iframeProxy);
+            iframes.add(iframe);
+        } catch {}
+    }
+
+    setInterval(() => {
+        document.querySelectorAll('iframe').forEach(hookIframe);
+    }, 100);
+
+    function handle(type, iframe) {
+        return {
+            get: function(target, property) {
+                if (property !== "postMessage") return Reflect.get(...arguments);
+                return function() {
+                    hook(arguments, type, iframe);
+                    return target[property].apply(target, arguments);
+                }
+            },
+        };
+    }
+
+    function hasProperty(value, key) {
+        return Object.prototype.hasOwnProperty.call(value, key);
+    }
+
+    function hookWindow(w, p) {
+        if (hasProperty(w, p)) {
+            if (!(w[p] instanceof Window)) return;
+            let real = w[p];
+            if (windows.has(real)) {
+                w[p] = windows.get(real);
+            } else {
+                w[p] = new Proxy(real, handle(p));
+                windows.set(real, w);
+            }
+        }
+    }
+
+    function hookWindows(w) {
+        if (!(w instanceof Window)) return;
+
+        hookWindow(w, "parent");
+        hookWindow(w, "opener");
+
+        if (hasProperty(w, "postMessage")) {
+            let real = w.postMessage;
+            if (windows.has(real)) {
+                w.postMessage = windows.get(real);
+            } else {
+                w.postMessage = function() {
+                    hook(arguments, "self");
+                    real.apply(this, arguments);
+                }
+                windows.set(real, w);
+            }
+        }
+    }
+
+    hookWindows(window);
 
 })();
