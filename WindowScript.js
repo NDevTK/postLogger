a81c3150878cb6da4e076a5b3f57689bf104f21c
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-11-08 14:20:15 +0000

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -73,11 +73,11 @@
     }
     
     if (window.parent !== window) {
-        window.parent.postMessage = new Proxy(window.parent.postMessage, handle('parent'));
+        window.parent = new Proxy(window.parent, handle('parent'));
     }
     
     if (window.opener) {
-        window.opener.postMessage = new Proxy(window.opener.postMessage, handle('opener'));
+        window.opener = new Proxy(window.opener, handle('opener'));
     }
     
     window.postMessage = new Proxy(window.postMessage, handle('self'));
