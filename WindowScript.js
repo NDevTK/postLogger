f2bd3be0f88ecfdf4153901b4ae7cb27642c042e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-11-14 19:58:48 +0000

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -52,7 +52,7 @@
     function whois(win, origin) {
         const source = useProxy(win);
         const target = displayOrigin(origin);
-        if (source.top === realTop) return 'top (' + target + ')';
+        if (source === realTop) return 'top (' + target + ')';
         if (source === window.parent && source !== window) return 'parent (' + target + ')';
         if (source === window.opener) return 'opener (' + target + ')';
 
