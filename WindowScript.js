8c8d6351917b0b127e70b7c2203cc42b12284f00
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-11-14 04:09:35 +0000

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -146,7 +146,7 @@
     
     window.parent = useProxy(window.parent, handle('parent'));
     window.opener = useProxy(window.opener, handle('opener'));
-    window.top = useProxy(window.opener, handle('top'));
+    window.top = useProxy(window.top, handle('top'));
     window.postMessage = useProxy(window.postMessage, handle('self'));
     window.open = openHook;
 })();
